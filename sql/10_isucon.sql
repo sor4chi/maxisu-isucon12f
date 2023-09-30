2147d3e4ef6937d4c85997e446755daa4f163127
sor4chi <sorakawa0218@gmail.com>
2023-09-30 15:29:34 +0900

    初期化時にDROPしちゃう

--- sql/10_isucon.sql
+++ sql/10_isucon.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `user_present_all_received_history` ADD INDEX `user_present_all_received_history_idx` (`user_id`, `present_all_id`);
-CREATE TABLE IF NOT EXISTS `user_presents_remaining` AS SELECT * FROM `user_presents` WHERE `deleted_at` IS NULL;
+DROP TABLE IF EXISTS `user_presents_remaining`;
+CREATE TABLE `user_presents_remaining` AS SELECT * FROM `user_presents` WHERE `deleted_at` IS NULL;
 ALTER TABLE `user_presents_remaining` ADD INDEX `user_presents_remaining_idx` (`user_id`);
