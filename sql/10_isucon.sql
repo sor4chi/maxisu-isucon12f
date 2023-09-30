ac16f13d749d0d62f3f12f2689363f6f14f644e3
sor4chi <sorakawa0218@gmail.com>
2023-09-30 15:15:32 +0900

    user_presents_remainingのuser_idにindexをはる

--- sql/10_isucon.sql
+++ sql/10_isucon.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `user_present_all_received_history` ADD INDEX `user_present_all_received_history_idx` (`user_id`, `present_all_id`);
 CREATE TABLE `user_presents_remaining` AS SELECT * FROM `user_presents` WHERE `deleted_at` IS NULL;
+ALTER TABLE `user_presents_remaining` ADD INDEX `user_presents_remaining_idx` (`user_id`);
