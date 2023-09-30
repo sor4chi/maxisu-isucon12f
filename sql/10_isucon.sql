77fb442b4a2df7aa2017bce505de6985f86c4ffc
sor4chi <sorakawa0218@gmail.com>
2023-09-30 15:12:06 +0900

    user_presents_remainingテーブルを作成

--- sql/10_isucon.sql
+++ sql/10_isucon.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `user_present_all_received_history` ADD INDEX `user_present_all_received_history_idx` (`user_id`, `present_all_id`);
+CREATE TABLE `user_presents_remaining` AS SELECT * FROM `user_presents` WHERE `deleted_at` IS NULL;
