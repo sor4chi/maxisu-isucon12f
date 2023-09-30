4572513bd00eb6cfaecce6e0c2ae556b5e19e666
sor4chi <sorakawa0218@gmail.com>
2023-09-30 16:19:49 +0900

    index貼る方向にシフト

--- sql/10_isucon.sql
+++ sql/10_isucon.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `user_present_all_received_history` ADD INDEX `user_present_all_received_history_idx` (`user_id`, `present_all_id`);
+
+ALTER TABLE `user_presents` ADD INDEX `user_present_idx` (`user_id`, `deleted_at`, `created_at` DESC);
